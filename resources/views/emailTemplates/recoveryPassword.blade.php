3e8ce20111fc18be544040b8a4c8513492d33241
David Leonardo Molina Ruíz <dmolina101@gmail.com>
2020-01-21 08:27:01 -0400

    update
    se corrigió un error ortografico

--- resources/views/emailTemplates/recoveryPassword.blade.php
+++ resources/views/emailTemplates/recoveryPassword.blade.php
@@ -49,7 +49,7 @@
         <img src="https://github.com/sofguar/carent/blob/master/public/images/logo-carent.png?raw=true" height="100" class="img-fluid logo" alt="">
     </div>
     <div id="wrapper_contenido">
-        <p>Se ha solicitado la recuperación de su contraseña, si usted no ha realizado esta acción por favor notificar al administrador dek sistema.</p>
+        <p>Se ha solicitado la recuperación de su contraseña, si usted no ha realizado esta acción por favor notificar al administrador del sistema.</p>
         <p><span class="titulo">Su contraseña de acceso al sistema es:</span></p>
         <p class="clave">{{ $clave }}</p>
     </div>
