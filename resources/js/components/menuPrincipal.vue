1d80e1d8e88238b966281952dd3dcbe350429473
David Leonardo Molina Ruíz <dmolina101@gmail.com>
2020-01-28 16:40:35 -0400

    update

--- resources/js/components/menuPrincipal.vue
+++ resources/js/components/menuPrincipal.vue
@@ -1,6 +1,6 @@
 <template>
   <nav id="menu-principal" class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand">
+    <a class="navbar-brand" href="/">
       <img src="/images/logo-carent-menu-expandido.png">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
