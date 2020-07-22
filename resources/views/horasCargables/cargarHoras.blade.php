fda07607f98081421703d93766e25f6a2c34c2e9
David Leonardo Molina Ruíz <dmolina101@gmail.com>
2020-07-22 16:42:54 -0400

    update

--- resources/views/horasCargables/cargarHoras.blade.php
+++ resources/views/horasCargables/cargarHoras.blade.php
@@ -9,7 +9,6 @@
 
         <title>.: CARENT :.</title>
         <link rel="shortcut icon" type="image/png" href="/images/favicon.png"/>
-        <link href="{{ mix('/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ mix('/css/fontawesome-free-5.12.0.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ mix('/css/cargarHoras.css') }}" rel="stylesheet" type="text/css">
 
