48d11e5343e46bda116a68050508ddadb10d70e2
serodriz <59027265+serodriz@users.noreply.github.com>
2020-08-06 13:33:23 -0400

    update

--- public/js/cargarHoras.js
+++ public/js/cargarHoras.js
@@ -36700,6 +36700,13 @@ var app = new Vue({
           self.horas_cargadas = self.infoHorasCargadas[i].horas_trabajadas + self.horas_cargadas;
         }
 
+        ;
+
+        if (self.permisoActualizar === false) {
+          self.cargar = 0;
+        }
+
+        ;
         self.loading = false;
       } else {
         throw "error";
@@ -36789,6 +36796,13 @@ var app = new Vue({
             message: message,
             show: true
           };
+          setTimeout(function () {
+            self.alertForm = {
+              "class": "",
+              message: "",
+              show: false
+            };
+          }, 2000);
         } else if (parseInt(horas_asignadas) < parseInt(horas_cargadas) + parseInt(self.form.horas_trabajadas.value) && self.cargar === 1) {
           var message = "Sobrepasaste el limite de horas asignadas. Si estas seguro volver hacer clic en Cargar Horas.";
           self.alertForm = {
@@ -36804,6 +36818,13 @@ var app = new Vue({
             message: message,
             show: true
           };
+          setTimeout(function () {
+            self.alertForm = {
+              "class": "",
+              message: "",
+              show: false
+            };
+          }, 2000);
         } else if (fechaN < "06/01/2010") {
           var message = "Debe introducir una fecha valida";
           self.alertForm = {
