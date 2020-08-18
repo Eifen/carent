8761fc297b5225bd450b476f481ed2448f5b0ea2
serodriz <59027265+serodriz@users.noreply.github.com>
2020-08-17 20:11:56 -0400

    update

--- resources/views/cliente/nuevoCliente.blade.php
+++ resources/views/cliente/nuevoCliente.blade.php
@@ -27,7 +27,7 @@
                         v-bind:disabled="formSearch.select.disabled"
                         v-model="formSearch.select.value"
                         v-on:change="tipoFiltro">
-                  <option value="" selected disabled>Agregar Socio encargado por</option>
+                  <option value="" selected disabled>Agregar Socio Por</option>
                   <option value="1">Código de usuario</option>
                   <option value="2">Cédula</option>
                   <option value="4">Primer o segundo nombre</option>
@@ -87,77 +87,6 @@
                   <div class="form-group col-12 col-sm-6" v-show="usuarios.mostrar">
                     <label>Nombre</label>
                     <input class="form-control" type="text" disabled v-bind:value="detalleUsuario.data.nombre">
-                  </div>
-            </form>
-          </form>
-          <form class="row">
-              <div class="form-group col-12 col-md-4">
-                <select class="form-control"
-                        v-bind:disabled="formSearchG.selectG.disabled"
-                        v-model="formSearchG.selectG.value"
-                        v-on:change="tipoFiltroG">
-                  <option value="" selected disabled>Agregar Gerente encargado por</option>
-                  <option value="1">Código de usuario</option>
-                  <option value="2">Cédula</option>
-                  <option value="4">Primer o segundo nombre</option>
-                  <option value="5">Primer o segundo apellido</option>
-                </select>
-              </div>
-              <div class="form-group col-12 col-md-6">
-                <input class="form-control inputSearch"
-                       ref="inputSearchG"
-                       type="text"
-                       v-bind:disabled="formSearchG.inputSearchG.disabled"
-                       v-on:keyup="evaluarCampoG('inputSearchG', $event)"
-                       v-model="formSearchG.inputSearchG.value">
-                <div class="mensaje"></div>
-              </div>
-              <div class="form-group col-12 col-md-2">
-                <!--Al hacer clic se invoca el metodo buscarG de crearCliente.js y abre la modal-->
-                <button class="btn btn-primary"
-                        type="button"
-                        v-bind:disabled="formSearchG.submitG.disabled"
-                        v-html="formSearchG.submitG.html"
-                        v-on:click="buscarG">
-                </button>
-              </div>
-              <div id="modal-detalle-usuarioG" class="modal fade" tabindex="-1" role="dialog" v-cloak>
-                <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-                  <div class="modal-content">
-                    <table class="table">
-                      <thead>
-                        <tr>
-                          <th scope="col">Código</th>
-                          <th scope="col">Cédula</th>
-                          <th scope="col">Nombre</th>
-                          <th scope="col">Cargo</th>
-                          <th scope="col"></th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <!-- Se llena la tabla con los valores que tiene usuariosG.registros obtenidos de crearCliente.js -->
-                        <tr v-for="usuario in usuariosG.registros">
-                          <th scope="row">@{{ usuario.codigo }}</th>
-                          <td>@{{ usuario.cedula }}</td>
-                          <td>@{{ usuario.nombre }}</td>
-                          <td>@{{ usuario.cargo }}</td>
-                          <td>
-                            <i class="fas fa-check-square" data-dismiss="modal" v-on:click="SelecionarUsuarioG(usuario.id, $event)"></i>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
-            <form class="row" v-if="!detalleUsuarioG.error">
-                  <div class="form-group col-12 col-sm-6" v-show="usuariosG.mostrar">
-                    <label>Código</label>
-                    <input class="form-control" type="text" disabled v-bind:value="detalleUsuarioG.data.codigo">
-                  </div>
-                  <div class="form-group col-12 col-sm-6" v-show="usuariosG.mostrar">
-                    <label>Nombre</label>
-                    <input class="form-control" type="text" disabled v-bind:value="detalleUsuarioG.data.nombre">
                   </div>
             </form>
           </form>
