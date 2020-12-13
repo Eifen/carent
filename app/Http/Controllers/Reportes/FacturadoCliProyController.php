8275b319bee5b734d181d863596d8a8e2b04dae5
David Leonardo Molina Ruíz <dmolina101@gmail.com>
2020-12-12 21:10:08 -0400

    update

--- app/Http/Controllers/Reportes/FacturadoCliProyController.php
+++ app/Http/Controllers/Reportes/FacturadoCliProyController.php
@@ -6,7 +6,7 @@ use App\Models\Reportes\facturadoCliProyModel;
 
 use App\Http\Controllers\Controller;
 
-class facturadoCliProyController extends Controller
+class FacturadoCliProyController extends Controller
 {
 
     function dataRepFacturadoCliProy(){
