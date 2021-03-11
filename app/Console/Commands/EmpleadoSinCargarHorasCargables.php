86a9ac161dc9eb365bb2a258b92997d29ee417cc
dmolina101 <dmolina101@gmail.com>
2021-03-10 21:28:01 -0400

    #402
    Update

---
+++ app/Console/Commands/EmpleadoSinCargarHorasCargables.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class EmpleadoSinCargarHorasCargables extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'registered:empleadoSinCargarHorasCargables';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Comando que permite saber que empleados no han cargado horas cargables en un tiempo determinado';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,9 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->call('App\Http\Controllers\Reportes\TotalHorasEmpController@repEmpSinCargarHoras')->everyMinute();
+        //$schedule->command('empleadoSinCargarHorasCargables')->everyMinute();
+
     }
 
     /**
