28c7d57af6dd21a92de3be8c44ef25fb85fcb8ff
David <dmolina101@gmail.com>
2022-10-14 20:08:15 -0400

    update

--- resources/js/usuario/listingUsers.vue
+++ resources/js/usuario/listingUsers.vue
@@ -1,6 +1,42 @@
 <template>
     <div class="row align-items-center justify-content-center wrapper-forms" v-cloak>
         <Filters @clearUsersList="clearUsersList" @showUsers="showUsers"/>
+        <div class="col-12" v-show="users.show">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Código</th>
+                        <th scope="col">Cédula</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Correo</th>
+                        <th scope="col">Estatus</th>
+                        <th scope="col"></th>
+                        <th scope="col" v-if="canUpdate"></th>
+                        <th scope="col" v-if="canUpdate">Asignar Menus</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="user in users.data">
+                        <th scope="row">{{ user.codigo }}</th>
+                        <td>{{ user.cedula }}</td>
+                        <td>{{ user.nombre }}</td>
+                        <td>{{ user.correo_principal }}</td>
+                        <td>{{ user.estatus }}</td>
+                        <td>
+                            <i class="fas fa-search-plus" v-on:click="mostrarDetalleUsuario(user.id, $event)"></i>
+                        </td>
+                        <td v-if="canUpdate">
+                            <a v-bind:href="'/formModificarUsuario/'+user.id" target="_self">
+                                <i class="far fa-edit"></i>
+                            </a>
+                        </td>
+                        <td>
+                            <i class="fas fa-user-edit" v-on:click="mostrarDetalleMenu(usuario.id, $event)"></i>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
@@ -15,9 +51,10 @@
         },
         data() {
             return {
-                usuarios: {
-                    mostrar: false,
-                    registros: []
+                canUpdate: false,
+                users: {
+                    data: [],
+                    show: false
                 }
             }
         },
@@ -27,12 +64,16 @@
         methods: {
 
             clearUsersList: function() {
-                this.usuarios.mostrar = false;
-                this.usuarios.registros = [];
+                this.users.show = false
+                this.users.data = []
             },
-            showUsers: function(users) {
-                console.log("buscar")
-                console.log(users)
+            mostrarDetalleUsuario: function() {},
+            showUsers: function(data) {
+
+                this.canUpdate = data.canUpdate
+                this.users.data = data.users
+                this.users.show = data.showUsers
+
             }
 
         }
