9604eccc67416feea220c3471410571bbe4af4c0
Eifen <carlosnavased@gmail.com>
2023-03-18 01:34:28 -0400

    Changes to be committed: 	new file:   app/Http/Controllers/Auth/ConfirmPasswordController.php 	new file:   app/Http/Controllers/Auth/ForgotPasswordController.php 	new file:   app/Http/Controllers/Auth/LoginController.php 	new file:   app/Http/Controllers/Auth/RegisterController.php 	new file:   app/Http/Controllers/Auth/ResetPasswordController.php 	new file:   app/Http/Controllers/Auth/VerificationController.php 	new file:   app/Http/Controllers/HomeController.php 	new file:   app/Models/LoginModel.php 	new file:   database/migrations/2014_10_12_100000_create_password_resets_table.php 	new file:   resources/js/Components/FontAwesome/FontAwesome.vue 	new file:   resources/js/ControlUsers/login.js 	new file:   resources/js/fontawesome/fontAwesomeInit.js 	new file:   resources/less/index.less 	new file:   resources/less/login/login.less 	new file:   resources/sass/_variables.scss 	new file:   resources/sass/app.scss 	new file:   resources/views/ControlUser/InitSession.blade.php 	new file:   resources/views/ControlUser/login.blade.php 	new file:   resources/views/auth/login.blade.php 	new file:   resources/views/auth/passwords/confirm.blade.php 	new file:   resources/views/auth/passwords/email.blade.php 	new file:   resources/views/auth/passwords/reset.blade.php 	new file:   resources/views/auth/register.blade.php 	new file:   resources/views/auth/verify.blade.php 	new file:   resources/views/home.blade.php 	new file:   resources/views/layouts/app.blade.php

---
+++ resources/js/Components/FontAwesome/FontAwesome.vue
@@ -0,0 +1,26 @@
+<template>
+        <FontAwesomeIcon :icon="stringIcon"></FontAwesomeIcon>
+</template>
+
+<script>
+//Importamos las librerias de fontawesome y vue
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLock } from '@fortawesome/free-solid-svg-icons'
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+/* Agregamos los iconos a la libreria*/
+library.add(faUser,faLock, faEye,faEyeSlash)
+
+export default {
+    props: {
+        stringIcon: String
+    },
+    components: { FontAwesomeIcon }
+}
+</script>
+
+<style>
+
+</style>
