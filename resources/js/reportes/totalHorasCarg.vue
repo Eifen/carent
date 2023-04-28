49e321e402bba7eb7b78e5da5920c99072a947a5
Eifen <80792817+Eifen@users.noreply.github.com>
2023-04-28 14:22:32 -0400

    Revert "#598"

--- resources/js/reportes/totalHorasCarg.vue
+++ resources/js/reportes/totalHorasCarg.vue
@@ -176,8 +176,8 @@
           </alert>
         </template>
         <template v-slot:cell(e)="data">
-           <i v-if="data.item.eficiencia=='eficiente'" class="fas fa-check" style="color: #40d44a;"></i>
-           <i v-if="data.item.eficiencia=='deficiente'" class="fas fa-times" style="color: #ce122e;"></i>
+           <i v-if="data.item.eficiencia" class="fas fa-check" style="color: #40d44a;"></i>
+           <i v-if="!data.item.eficiencia" class="fas fa-times" style="color: #ce122e;"></i>
         </template>
         <template v-slot:custom-foot v-if="tabla.registros.length > 0">
           <b-tr>
