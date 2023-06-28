f35c7d3037c4f2c73ad10f8c383ce6dbe4d7c7f7
kislev-owo <kislev_083@hotmail.com>
2023-06-28 17:37:08 -0400

    Changes to be committed:

--- resources/views/layouts/navBar/proyectos.blade.php
+++ resources/views/layouts/navBar/proyectos.blade.php
@@ -1,8 +1,13 @@
 <div>
     <a class="header-nav-links-item-link">Proyectos <font-awesome string-icon="fa-solid fa-caret-down"></font-awesome></a>
-    <div class="header-nav-links-item-dropdown" :style="controlDropdown.projects.style" @mouseover="openDropDown('projects')" @mouseout="closeDropDown('projects')">
-        <div class="header-nav-links-item-dropdown-content"><a href="{{URL::route('projects')}}">Control de proyectos</a></div>
-        <div class="header-nav-links-item-dropdown-content"><a href="{{URL::route('assign')}}">Asignacion de proyectos</a></div>
+    <div class="header-nav-links-item-dropdown" :style="controlDropdown.projects.style"
+        @mouseover="openDropDown('projects')" @mouseout="closeDropDown('projects')">
+        <div class="header-nav-links-item-dropdown-content"><a href="{{ URL::route('projects') }}">Control de
+                proyectos</a></div>
+        <div class="header-nav-links-item-dropdown-content"><a href="{{ URL::route('assign') }}">Asignacion de
+                proyectos</a></div>
         <div class="header-nav-links-item-dropdown-content"><a href="/projects/register-hours">Carga de horas</a></div>
+        <div class="header-nav-links-item-dropdown-content"><a href="/projects/close-projects">Cierre de proyectos</a>
+        </div>
     </div>
 </div>
