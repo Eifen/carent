648f123538bdb62aa635a4ab64ef9869a882c61a
Eifen <carlosnavased@gmail.com>
2023-09-27 17:48:12 -0400

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   resources/js/Components/ListingCrudExcel.js

--- resources/js/Components/ListingCrudExcel.js
+++ resources/js/Components/ListingCrudExcel.js
@@ -512,6 +512,7 @@ export const ListingMixinMethods = {
             }
             //Hacemos un foreach de la lista por area
             listExcel[1].forEach((field) => {
+                console.log(hourRef.realAdmon,field.tot_hor_admon)
                 hourRef.refProy += parseFloat(field.hor_esp_proy);
                 hourRef.refAdmon += parseFloat(field.hor_esp_admon);
                 hourRef.realAdmon += parseFloat(field.tot_hor_admon);
