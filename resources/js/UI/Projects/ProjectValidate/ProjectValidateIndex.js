403dbfd7b390557e6cd2f96c84a8fc762b8ff17e
Eifen <carlosnavased@gmail.com>
2025-06-10 11:32:27 -0400

    Changes to be committed: 	modified:   resources/js/UI/Projects/ProjectValidate/ProjectValidateIndex.js

--- resources/js/UI/Projects/ProjectValidate/ProjectValidateIndex.js
+++ resources/js/UI/Projects/ProjectValidate/ProjectValidateIndex.js
@@ -35,7 +35,7 @@ const validateApp = createApp({
                 select3: "Fecha desde",
                 select4: "Fecha hasta",
                 select5: "Estatus",
-                select6: "Area"
+                select6: "Areas"
             },
             //Seleccionamos la tabla
             tableTarget: "vw_projects_admin_preview",
